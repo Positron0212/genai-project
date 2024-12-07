fb307709227f8e0016c17215471d81cdc6d57ccd
Dhanraj Singh <99534842+sdhanraj300@users.noreply.github.com>
2024-12-07 11:14:52 +0530

    Update Sidebar.jsx

--- frontend/src/components/chat/Sidebar.jsx
+++ frontend/src/components/chat/Sidebar.jsx
@@ -49,7 +49,7 @@ export default function Sidebar({
     try {
       const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API);
       const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
+        model: "tunedModels/mental-2-zjoksaqc97v1",
         temperature: 0.5,
         maxTokens: 100,
       });
